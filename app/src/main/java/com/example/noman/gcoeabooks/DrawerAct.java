755300f510e03774a2f6f8243cf229ed8caba70d
Noman <knoman066@gmail.com>
2018-05-06 12:10:19 +0530

    Added the Navigation Drawer

---
+++ app/src/main/java/com/example/noman/gcoeabooks/DrawerAct.java
@@ -0,0 +1,13 @@
+package com.example.noman.gcoeabooks;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class DrawerAct extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_drawer);
+    }
+}
--- app/src/main/java/com/example/noman/gcoeabooks/MainActivity.java
+++ app/src/main/java/com/example/noman/gcoeabooks/MainActivity.java
@@ -1,15 +1,32 @@
 package com.example.noman.gcoeabooks;
 
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.MenuItem;
 import android.widget.Toolbar;
 
 public class MainActivity extends AppCompatActivity {
-
+private DrawerLayout sdrawerlayout;
+private ActionBarDrawerToggle sToggle;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Toolbar toolbar;
+        sdrawerlayout=findViewById(R.id.main_draw);
+        sToggle=new ActionBarDrawerToggle(this,sdrawerlayout,R.string.open,R.string.close);
+        sdrawerlayout.addDrawerListener(sToggle);
+        sToggle.syncState();;
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        if(sToggle.onOptionsItemSelected(item)){
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
     }
 }
+
